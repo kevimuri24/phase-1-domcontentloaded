bd72708b95238a03e502afef91f0f9ea3e8f336f
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-07-17 21:35:56 +0300

    Add an Event Listenser

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () =>{
+        let test = document.querySelector('#text')
+        test.textContent = "This is really cool!"
+})
